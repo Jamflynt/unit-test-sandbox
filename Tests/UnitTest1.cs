75027be0a25076b10c218eb32729bc4afe1eeb32
Jamie Flynt <Jamflynt@gmail.com>
2024-05-02 16:34:02 -0400

    Forgot to push this up. Theory.

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -10,4 +10,19 @@ public class UnitTest1
     {
         Assert.Equal(4, Add(2, 2));
     }
+    
+    [Fact]
+    public void FailingTest()
+    {
+        Assert.Equal(5, Add(2, 2));
+    }
+    
+    [Theory]
+    [InlineData(2, 2, 4)]
+    [InlineData(3, 3, 6)]
+    [InlineData(2, 2, 5)]
+    public void MyTheory(int x, int y, int sum)
+    {
+        Assert.Equal(sum, Calculator.Add(x, y));
+    }
 }
--- Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1464a56790da44a27f8641385495c4c0bf11c5d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
